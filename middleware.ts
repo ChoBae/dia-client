ac1c4dd3e066299ac64f9c767e4ca726faaecf66
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2024-03-30 14:21:36 +0900

    [fix]버셀 배포 에러 수정

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,8 @@
 import { NextRequest, NextResponse, userAgent } from "next/server";
-import { updateSession, getSession } from "./authLib";
-import { cookies } from "next/headers";
 
 export async function middleware(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
-  const session = await getSession();
-  // console.log('session', session.accessToken);
   requestHeaders.set("user-agent", userAgent(request).ua);
-  if (session) {
-    requestHeaders.set("authorization", session.accessToken);
-  }
-  // console.log("requestHeaders", requestHeaders);
   const response = NextResponse.next({
     request: {
       // New request headers
