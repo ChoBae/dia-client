d3e84bc625c568b044124a3dd6e82ef216b79c5b
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2024-03-30 14:36:31 +0900

    [fix]vercel 설정 수정

--- next.config.js
+++ next.config.js
@@ -26,7 +26,7 @@ const nextConfig = {
       fallback: [
         {
           source: "/proxy/:path*",
-          destination: `https://staging.api.d-i-a.run/api/:path*`,
+          destination: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/:path*`,
         },
       ],
     };
