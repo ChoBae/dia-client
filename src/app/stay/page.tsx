2c5a672c018e60e0c9c560117a0b2badab8af95b
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2024-05-21 23:07:26 +0900

    [chore]테스트페이지 생성

--- src/app/stay/page.tsx
+++ src/app/stay/page.tsx
@@ -1,4 +1,7 @@
-import Main from "./Main";
+import dynamic from "next/dynamic";
+const Main = dynamic(() => import("./Main"), {
+  ssr: false,
+});
 export default function Home() {
   return <Main />;
 }
